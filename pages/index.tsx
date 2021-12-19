78a1659cf28855c9b2b19f18b33dd42071c5a96f
kriziu <brunodzi07@gmail.com>
2021-12-19 23:52:42 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,11 @@
 import type { NextPage } from 'next';
-import { BsTelephoneFill } from 'react-icons/bs';
 
-import { Header4 } from 'components/Simple/Headers';
-import ChatContainer from 'components/Chat/ChatContainer';
-import { AvatarSmall } from 'components/Simple/Avatars';
-import { Flex } from 'components/Simple/Flex';
+import { Header1 } from 'components/Simple/Headers';
 
 const Home: NextPage = () => {
   return (
     <>
-      <Flex
-        style={{
-          width: '100%',
-          justifyContent: 'space-between',
-          padding: '0 3rem',
-        }}
-      >
-        <Flex style={{ marginLeft: '4rem' }}>
-          <AvatarSmall active />
-          <Header4
-            style={{
-              width: 'min-content',
-              textAlign: 'left',
-              marginLeft: '1rem',
-            }}
-          >
-            Bruno Dzięcielski
-          </Header4>
-        </Flex>
-        <div>
-          <BsTelephoneFill />
-        </div>
-      </Flex>
-
-      <ChatContainer />
+      <Header1>Index</Header1>
     </>
   );
 };

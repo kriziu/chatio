d9555fa45b674cf5bf6fcc771f81ae5a559050ce
kriziu <brunodzi07@gmail.com>
2022-02-09 14:57:14 +0100

    fix

--- pages/api/pusher/block.ts
+++ pages/api/pusher/block.ts
@@ -3,13 +3,23 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { pusher } from 'common/lib/pusher';
 import checkAdmin from 'backend/middlewares/checkAdmin';
 import getUserId from 'backend/middlewares/getUserId';
+import connectionModel from 'backend/models/connection.model';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const _id = getUserId(req);
   const { connectionId } = req.query;
 
-  const connection = await checkAdmin(req, connectionId as string);
-  if (!connection) return res.status(403).end();
+  const connection = await connectionModel.findById(connectionId);
+
+  if (!connection) {
+    return res.status(404).end();
+  }
+  console.log(connection);
+  if (connection?.group) {
+    const testConnection = await checkAdmin(req, connectionId as string);
+
+    if (!testConnection) return res.status(403).end();
+  }
 
   if (connection.blocked.yes) {
     if (connection.blocked.by?.equals(_id)) {
--- pages/api/pusher/delete.ts
+++ pages/api/pusher/delete.ts
@@ -3,13 +3,22 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { pusher } from 'common/lib/pusher';
 import messageModel from 'backend/models/message.model';
 import checkAdmin from 'backend/middlewares/checkAdmin';
-import getUserId from 'backend/middlewares/getUserId';
+import connectionModel from 'backend/models/connection.model';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const { connectionId } = req.query;
 
-  const connection = await checkAdmin(req, connectionId as string);
-  if (!connection) return res.status(403).end();
+  const connection = await connectionModel.findById(connectionId);
+
+  if (!connection) {
+    return res.status(404).end();
+  }
+  console.log(connection);
+  if (connection?.group) {
+    const testConnection = await checkAdmin(req, connectionId as string);
+
+    if (!testConnection) return res.status(403).end();
+  }
 
   await connection.delete();
   await messageModel.deleteMany({ connectionId });
